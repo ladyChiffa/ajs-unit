18992429a606df0f9005705da544e09668d2cdb3
Oksana (Kes) Milenina <OkMilenina@yandex.ru>
2026-03-10 11:06:38 +0300

    eslint config fix

--- src/__test__/health.test.js
+++ src/__test__/health.test.js
@@ -1,22 +1,21 @@
 import getHealthStatus from '../health.js';
 
-test("healthStatus, 51", () => {
-    const status = getHealthStatus({name: "Max", health:51});
+test('healthStatus, 51', () => {
+    const status = getHealthStatus({name: 'Max', health:51});
     expect(status).toBe('healthy');
 });
 
 const dataList = [
-    [{name: "Max", health:100}, 'healthy'],
-    [{name: "Max", health:51}, 'healthy'],
-    [{name: "Max", health:50}, 'wounded'],
-    [{name: "Max", health:32}, 'wounded'],
-    [{name: "Max", health:15}, 'wounded'],
-    [{name: "Max", health:14}, 'critical'],
-    [{name: "Max", health:8}, 'critical']
+    [{name: 'Max', health:100}, 'healthy'],
+    [{name: 'Max', health:51}, 'healthy'],
+    [{name: 'Max', health:50}, 'wounded'],
+    [{name: 'Max', health:32}, 'wounded'],
+    [{name: 'Max', health:15}, 'wounded'],
+    [{name: 'Max', health:14}, 'critical'],
+    [{name: 'Max', health:8}, 'critical']
 ];
 
-const prefix = 'testing healthStatus for %s';
-test.each(dataList)(prefix, (myCharacter, status) => {
+test.each(dataList)('testing healthStatus for %s', (myCharacter, status) => {
     const result = getHealthStatus(myCharacter);
     expect(result).toBe(status);
 });
